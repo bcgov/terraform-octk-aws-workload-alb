7f36731039ddb5ddafabd146ac380cd67394dab7
Warren Uniewski <warren@autimo.com>
2020-12-28 13:44:41 -0800

    Set required_provider block

--- module/main.tf
+++ module/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.11"
+    }
+  }
+}
+
 locals {
   environment            = title(var.environment)
   vpc_name               = "${local.environment}_vpc"
